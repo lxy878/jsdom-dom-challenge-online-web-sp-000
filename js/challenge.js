7427bb65ffc8c280175619000ad6e3528e72af1f
Xiaoyi Liu <lxy878@gmail.com>
2020-08-13 10:50:20 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,1 +1,29 @@
-let playing = true;
+let playing = true, count;
+
+// const timer = () => {
+//   return setInterval(function () {
+//     count += 1;
+//     counter.textContent = count;
+//   }, 1000);
+// }
+//
+// interval = timer();
+
+document.addEventListener('click', (event)=>{
+  if(event.target.id === 'plus'){
+    console.log('plus');
+  }else if (event.target.id === 'minus'){
+    console.log('plus1');
+  }else if (event.target.id === 'pause'){
+    const button = document.getElementById('pause');
+    if(playing){
+      button.textContent = 'resume';
+      playing = false;
+    }else{
+      button.textContent = 'pause';
+      playing = false;
+    }
+  }else if (event.target.id === 'heart'){
+    console.log('plus3');
+  }
+});
