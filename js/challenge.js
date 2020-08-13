fa3d3275735c71bd6635738d11ec3defbeac0081
Xiaoyi Liu <lxy878@gmail.com>
2020-08-13 10:14:08 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,27 @@
+// const timer = () => {
+//   return setInterval(function () {
+//     count += 1;
+//     counter.textContent = count;
+//   }, 1000);
+// }
+//
+// interval = timer();
+
+document.addEventListener('click', (event)=>{
+  if(event.target.id === 'plus'){
+    console.log('plus');
+  }else if (event.target.id === 'minus'){
+    console.log('plus1');
+  }else if (event.target.id === 'pause'){
+    const button = document.getElementById('pause');
+    if(playing){
+      button.textContent = 'resume';
+      playing = false;
+    }else{
+      button.textContent = 'pause';
+      playing = false;
+    }
+  }else if (event.target.id === 'heart'){
+    console.log('plus3');
+  }
+});
