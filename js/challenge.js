440cc7c1089e6026538745259725ed14391e3da5
Xiaoyi Liu <lxy878@gmail.com>
2020-08-13 10:36:43 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -17,7 +17,8 @@ let interval = timer();
 
 document.addEventListener('click', (event)=>{
   if(event.target.id === 'plus'){
-    console.log('plus');
+    count += 1;
+    counter.textContent = count;
   }else if (event.target.id === 'minus'){
     console.log('plus1');
   }else if (event.target.id === 'pause'){
