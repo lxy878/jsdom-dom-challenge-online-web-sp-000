a547bb2547575e5d57f5dd1d5dae68105397e52a
Xiaoyi Liu <lxy878@gmail.com>
2020-08-13 10:29:00 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,6 +1,6 @@
 let playing = true, count = 0;
 const button_pause = document.getElementById('pause');
-const button_pause = document.getElementById('pause');
+const button_plus = document.getElementById('pause');
 const button_pause = document.getElementById('pause');
 const button_pause = document.getElementById('pause');
 
