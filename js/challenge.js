b9734902efd91ebeb73dd41184486656371ebd44
Xiaoyi Liu <lxy878@gmail.com>
2020-08-13 10:31:12 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,8 +1,4 @@
 let playing = true, count = 0;
-const button_pause = document.getElementById('pause');
-const button_plus = document.getElementById('plus');
-const button_minus = document.getElementById('minus');
-const button_pause = document.getElementById('pause');
 
 
 const timer = () => {
